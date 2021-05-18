6d12c8e430cb63e64e9b434c661ddaad441783bc
Chris Yoong <chris.yoong@digital.cabinet-office.gov.uk>
2021-05-18 17:38:34 +0100

    Update "find via postcode" pages.
    Once gem_layout used, adjustments to pages that require a user to input a PostCode. Removing redundant CSS aligning width with Design System Guidance

--- app/controllers/travel_advice_controller.rb
+++ app/controllers/travel_advice_controller.rb
@@ -7,9 +7,9 @@ class TravelAdviceController < ApplicationController
     @presenter = TravelAdviceIndexPresenter.new(@content_item)
 
     respond_to do |format|
-      format.html { 
+      format.html do
         slimmer_template "gem_layout"
-       }
+      end
       format.atom do
         set_expiry(5.minutes)
         headers["Access-Control-Allow-Origin"] = "*"
